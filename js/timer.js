a7d4b3f156afa2cb1940c5a5238d78169335b235
und3rdg <und3rdg@gmail.com>
2017-05-12 17:32:27 +0100

    Rewrite to objective code

--- js/timer.js
+++ js/timer.js
@@ -1,71 +1,62 @@
-$(document).ready(function(){ 
+//$(document).ready(function(){ 
 
-
-// colors
-function colorIddle(){
-    $("time").css("color", "black");
-}
-
-function colorReady(){
-    $("time").css("color", "lightgreen");
-}
-
-function colorRuning(){
-    $("time").css("color", "blue");
-}
-
-
-var timeRuning = false;
-var down = {};
-var trigerStep = 1;
-
-function spaceDown(e){
-    var keycode = (e.keyCode ? e.keyCode : e.which);
-    if(e.keyCode == 32){
-            // ready to start time
-        if(trigerStep % 3 == 1){
-            if(down['32'] == null){    
-                trigerStep++;
-                console.log("Ready to start // var timeRuning = " + timeRuning + " // trigerStep = " + trigerStep %3);
-                colorReady();
-                down['32'] = true;
+var Timer = {
+    iddle: function(){ // stop
+        $("time").css("color", "black")
+        console.log("stop time // trigerStep = " + this.trigerStep %3);
+    },
+    ready: function(){
+        $("time").css("color", "lightgreen");
+        console.log("Ready to start // trigerStep = " + this.trigerStep %3);
+    },
+    runing: function(){
+        $("time").css("color", "blue")
+        console.log("start time // trigerStep = " + this.trigerStep %3)
+    },
+    trigerStep: 1,
+    // 0 start, spaceUp //ready to stop on keyDown
+    // 1 stop, spaceDown // do noting on keyUp
+    // 2 ready, spaceDown // ready to start on keyUp
+    down: {}, // key down spamming fix
+    initKey: 32, // space
+    spaceDown: function(e){
+        var keycode = (e.keyCode ? e.keyCode : e.which);
+        if(e.keyCode == this.initKey){
+                // ready to start time
+            if(this.trigerStep % 3 == 1){
+                if(this.down[this.initKey] == null){    
+                    this.trigerStep++;
+                    this.ready();
+                }
             }
-        }
-            // stop time
-        if(trigerStep % 3 == 0){
-            if(down['32'] == null){
-                trigerStep++;
-                console.log("stop time // var timeRuning = " + timeRuning + " // trigerStep = " + trigerStep %3);
-                colorIddle();
-                down['32'] = true;
+                // stop time
+            if(this.trigerStep % 3 == 0){
+                if(this.down[this.initKey] == null){
+                    this.trigerStep++;
+                    this.iddle();
+                }
             }
-        }
-    }
-}
-
-function spaceUp(e){
-    var keycode = (e.keyCode ? e.keyCode : e.which);
-    if(e.keyCode == 32){
+            this.down[this.initKey] = true;
+        };
+    },
+    spaceUp: function(e){
+        var keycode = (e.keyCode ? e.keyCode : e.which);
+        if(e.keyCode == this.initKey){
             // start time
-            if(trigerStep % 3 == 2){
-                trigerStep++;
-                console.log("start time // var timeRuning = " + timeRuning + " // trigerStep = " + trigerStep %3)
-                colorRuning();
-                down['32'] = null;
-            } else {
-                down['32'] = null;
-            }
+            if(this.trigerStep % 3 == 2){
+                this.trigerStep++;
+                this.runing();
+            } 
+            this.down[this.initKey] = null;
+        }
+    },
+    init: function(){
+        $( document ).keydown(this.spaceDown.bind(this));
+        $( document ).keyup(this.spaceUp.bind(this));
     }
-}
+};
 
-$( document ).keydown(spaceDown);
-$( document ).keyup(spaceUp);
-
+Timer.init();
 
 
-
-
-
-
-});
-
+//});
