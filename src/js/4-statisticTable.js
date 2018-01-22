bf8a201a649bfa3fa3a31b5de086c824162306b8
und3rdg <und3rdg@gmail.com>
2018-01-22 21:13:49 +0000

    [js] convert ms to sec in record's table on frontend

--- src/js/4-statisticTable.js
+++ src/js/4-statisticTable.js
@@ -3,11 +3,7 @@ function top1(arr){
   var top1 = bestTime(legitTimesArray)
 
   var celBest = $(".top1 td:nth-child(2)")
-  // var celDate = $(".top1 td:nth-child(3)")
-  // var celcurrent =  $(".top1 td:nth-child(4)")
 
-  celBest.text(top1.times_ms)
-  // celBest.text("44.444")
-  celDate.text(top1.date)
+  celBest.text(top1.times_ms.toString().convTime())
 
 }
