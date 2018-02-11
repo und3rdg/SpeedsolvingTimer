7e3b798616bcb663b644a36924c6b18f642cff69
und3rdg <und3rdg@gmail.com>
2018-02-11 15:59:38 +0000

    [sh] ECHO ECHo ECho Echo echo ech.. e..

--- publishDist.sh
+++ publishDist.sh
@@ -8,10 +8,19 @@ distPath=~/Code/sstimer/dist/
 ### REMOTE ###
 publishPath=sstimer.tk
 
+echo "     ?> cd $distPath"
 cd $distPath
+
+echo "     ?> add."
 git add .
+
+echo "     ?> git cm -m "$commitMsg""
 git cm -m "$commitMsg"
+
+echo "     ?> git push"
 git push
 
+echo "     ?> cd -"
 cd -
-echo '* all done'
+
+echo "     ?> * all done"
